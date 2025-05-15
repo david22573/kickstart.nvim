e446d0d77bc1abc219b340db9260a28525312f2e
David Rivero <davidmiguel22573@gmail.com>
2025-05-15 04:04:34 -0700

    add enter for accept and tabs for selection

--- init.lua
+++ init.lua
@@ -827,6 +827,9 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<Enter>'] = { 'accept' },
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
